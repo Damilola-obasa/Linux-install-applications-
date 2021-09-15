011892c2bf85daa26f3c49559221e96a08e92b20
Damilola-obasa <79476489+Damilola-obasa@users.noreply.github.com>
2021-09-15 19:33:42 -0400

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,10 @@
+ #! /bin/bash
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+
+sudo apt update
+sudo apt install teams
